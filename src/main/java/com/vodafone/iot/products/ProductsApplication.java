ee78b044c49772fe1eb6c9a4652fcb8fab57be22
mahmoudragab <mahmoudragab726@gmail.com>
2021-09-05 13:58:32 +0200

    fix swagger-UI redirection issue

--- src/main/java/com/vodafone/iot/products/ProductsApplication.java
+++ src/main/java/com/vodafone/iot/products/ProductsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
 @EnableWebMvc
@@ -19,6 +20,11 @@ public class ProductsApplication {
 		DispatcherServlet ds = new DispatcherServlet();
 		ds.setThrowExceptionIfNoHandlerFound(true);
 		return ds;
+	}
+
+	@Bean
+	public InternalResourceViewResolver defaultViewResolver() {
+		return new InternalResourceViewResolver();
 	}
 
 }
